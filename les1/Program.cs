97ef22c424ddd428b99e2d3d0487e5b25f923557
Alina <ajweis777@yandex.ru>
2022-11-29 13:49:43 +0300

    22

---
+++ les1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write(" N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string result ="";
+while(n>0)
+{
+result=n%2+result;
+
+n/=2;
+
+}
+Console.Write(result);
